187763c1afa138b51a96040503743e45c44b28fc
Sakti <icak_wicaksono@ymail.com>
2019-09-30 22:55:23 +0700

    Improve code, change path cloudinary, and update ruby version

--- app/controllers/waifudbs_controller.rb
+++ app/controllers/waifudbs_controller.rb
@@ -4,21 +4,25 @@ require 'open-uri'
 class WaifudbsController < ApplicationController
 
   def random_waifu
-    page = rand(1..11)
+    page = rand(1..13)
     url = page == 1 ? 'http://jurnalotaku.com/?s=%5Bwaifu+wednesday%5D' : "http://jurnalotaku.com/page/#{page}/?s=%5Bwaifu+wednesday%5D"
-    waifus = Nokogiri::HTML(open(url))
-    names = waifus.css('.article-inner-wrapper').css('.cover').css('img').map { |name| name['alt'].sub('[Waifu Wednesday] ','') if name['alt'].include?('[Waifu Wednesday]')}
-    images = waifus.css('.article-inner-wrapper').css('.cover').css('img').map { |img| img['src'] }
-    names.uniq!
-    names.delete_at(names.index(nil))
-    index = rand(0..names.size)
-    @waifudb = Waifudb.find_by(name: names[index])
+    data = Nokogiri::HTML(open(url))
+
+    waifus = data.css(".article-inner-wrapper").css(".cover").css("img").select { |name|
+      name['alt'] if name['alt'].include?("[Waifu Wednesday]")
+    }.map { |waifu| [waifu['alt'].sub("[Waifu Wednesday] ",""), waifu['src']] }
+
+    waifu = waifus.sample
+    name = waifu[0]
+    img =  waifu[1]
+
+    @waifudb = Waifudb.find_by(name: name)
     if @waifudb.blank?
-      upload = Cloudinary::Uploader.upload(images[index].to_s,
+      upload = Cloudinary::Uploader.upload(img.to_s,
                                            use_filename: true,
-                                           folder: 'Waifus/',
-                                           public_id: names[index].gsub(' ','-').to_s)
-      @waifudb = Waifudb.new(name: names[index], imgwaifu: upload['url'].sub('http','https'))
+                                           folder: 'Waifu_Collections/',
+                                           public_id: name.gsub(' ','-').to_s)
+      @waifudb = Waifudb.new(name: name, imgwaifu: upload['url'].sub('http','https'))
       @waifudb.save
     end
     render json: @waifudb, status: :ok
